8af42a7355601755d99d843f1ceffa64a35621bc
shivangi806 <97212405+shivangi806@users.noreply.github.com>
2024-01-29 07:30:17 +0000

    dataTypes and variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,10 @@
+// we will do investigation
+
+const accountId = 12345678;
+let emailId = "shivangichandra806@gmail.com";
+var password = "12345";
+accCity = "Patna";
+let myName ;
+// no use of var nowdays
+
+console.table([accountId , emailId , password , accCity , myName]);
---
+++ 01_basics/dataTypes.js
@@ -0,0 +1,4 @@
+"use strict" // to use newer version of java script
+// btw sb automatic new version me hi chalta hai 
+// alert(3+3);   // not defined in new version  we are using node.js not browser
+
