62f6e51dc4162e60f61c81caf3939dfd096da8f5
shivangi806 <97212405+shivangi806@users.noreply.github.com>
2024-02-04 18:18:12 +0000

    function

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,50 @@
-console.log("shivangi");
+// console.log("shivangi");
+
+let a = 89;
+var b = 90;
+var b = 99;
+// let a = 777;
+// console.log(b);
+
+
+const user = {
+    username : "shivangi",
+    phone : 90000,
+    msg :  function(){
+        // console.log(this.username);
+        // return this.username;
+    }
+}
+
+
+user.username = "ppppp";
+// console.log(user.msg);
+user.msg();
+
+// const add = function(){
+//     return 8+9;
+// }
+
+
+// using arrow function 
+// const add = () => {
+//     return 8+9;
+// }
+
+// arrow and return in brace
+const add = () =>(8+9);
+
+// console.log(add());
+
+function fun() {
+    return 90;
+}
+
+(function fun(){
+    console.log(9000000000);
+})(); // semicolon req
+
+(()=>{
+    console.log(299999);
+})()
+
